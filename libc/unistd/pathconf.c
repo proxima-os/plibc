9dfdc6e1adc6e8c4a89f940be9433ebee3fd4979
monkuous <84319635+monkuous@users.noreply.github.com>
2025-05-16 20:02:13 +0200

    unistd: implement fpathconf, pathconf, sysconf

--- libc/unistd/pathconf.c
+++ libc/unistd/pathconf.c
@@ -1,6 +1,17 @@
 #include "compiler.h"
 #include "unistd.h"
+#include <errno.h>
 
 EXPORT long pathconf(const char *path, int name) {
-    STUB();
+    switch (name) {
+    case _PC_CHOWN_RESTRICTED: return _POSIX_CHOWN_RESTRICTED;
+    case _PC_LINK_MAX: return -1;
+    case _PC_MAX_CANON: return -1;
+    case _PC_MAX_INPUT: return -1;
+    case _PC_NAME_MAX: return -1;
+    case _PC_NO_TRUNC: return _POSIX_NO_TRUNC;
+    case _PC_PIPE_BUF: return -1;
+    case _PC_VDISABLE: return _POSIX_VDISABLE;
+    default: errno = EINVAL; return -1;
+    }
 }
--- libc/unistd/sysconf.c
+++ libc/unistd/sysconf.c
@@ -1,6 +1,21 @@
 #include "compiler.h"
 #include "unistd.h"
+#include <errno.h>
+#include <limits.h>
+#include <time.h>
 
 EXPORT long sysconf(int name) {
-    STUB();
+    switch (name) {
+    case _SC_ARG_MAX: return -1;
+    case _SC_CHILD_MAX: return -1;
+    case _SC_CLK_TCK: return CLK_TCK;
+    case _SC_JOB_CONTROL: return _POSIX_JOB_CONTROL;
+    case _SC_NGROUPS_MAX: return -1;
+    case _SC_OPEN_MAX: return -1;
+    case _SC_SAVED_IDS: return _POSIX_SAVED_IDS;
+    case _SC_STREAM_MAX: return -1;
+    case _SC_TZNAME_MAX: return -1;
+    case _SC_VERSION: return _POSIX_VERSION;
+    default: errno = EINVAL; return -1;
+    }
 }
