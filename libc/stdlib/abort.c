9a449a5b00cc5647b08c305d529ced616b89a4a8
monkuous <84319635+monkuous@users.noreply.github.com>
2025-05-16 11:20:22 +0200

    abort: send signal to thread instead of process

--- libc/stdlib/abort.c
+++ libc/stdlib/abort.c
@@ -1,11 +1,12 @@
 #include "compiler.h"
 #include "stdlib.h"
 #include <hydrogen/process.h>
+#include <hydrogen/thread.h>
 #include <signal.h>
 
 #undef abort
 
 EXPORT void abort(void) {
-    raise(SIGABRT);
+    hydrogen_thread_send_signal(HYDROGEN_THIS_THREAD, SIGABRT);
     hydrogen_process_exit(127);
 }
