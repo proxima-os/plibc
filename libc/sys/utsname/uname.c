bec1d861a02da182098c394e43fa05a852c6254e
monkuous <84319635+monkuous@users.noreply.github.com>
2025-05-16 11:49:00 +0200

    utsname: implement uname

--- libc/sys/utsname/uname.c
+++ libc/sys/utsname/uname.c
@@ -1,6 +1,38 @@
 #include "compiler.h"
 #include "sys/utsname.h"
+#include <errno.h> /* IWYU pragma: keep */
+#include <hydrogen/hydrogen.h>
+#include <hydrogen/types.h>
+#include <string.h>
+
+static void terminate(char *buffer, size_t size, size_t full_size) {
+    buffer[full_size < size ? full_size : size] = 0;
+}
+
+static void fill_buffer(char *buffer, size_t size, size_t (*func)(void *, size_t)) {
+    terminate(buffer, size, func(buffer, size));
+}
+
+static size_t get_machine_name(void *buffer, size_t size) {
+    static const char name[] = "x86_64";
+    size_t full_size = sizeof(name) - 1;
+    memcpy(buffer, name, full_size < size ? full_size : size);
+    return full_size;
+}
 
 EXPORT int uname(struct utsname *name) {
-    STUB();
+    hydrogen_ret_t ret = hydrogen_get_host_name(name->nodename, sizeof(name->nodename) - 1);
+
+    if (unlikely(ret.error)) {
+        errno = ret.error;
+        return -1;
+    }
+
+    terminate(name->nodename, sizeof(name->nodename), ret.integer);
+    fill_buffer(name->machine, sizeof(name->machine), get_machine_name);
+    fill_buffer(name->release, sizeof(name->release), hydrogen_get_kernel_release);
+    fill_buffer(name->sysname, sizeof(name->sysname), hydrogen_get_kernel_name);
+    fill_buffer(name->version, sizeof(name->version), hydrogen_get_kernel_version);
+
+    return 0;
 }
--- public/sys/utsname.h
+++ public/sys/utsname.h
@@ -12,11 +12,11 @@ extern "C" {
 #endif
 
 struct utsname {
-    char machine[64];
-    char nodename[64];
-    char release[64];
-    char sysname[64];
-    char version[64];
+    char machine[65];
+    char nodename[65];
+    char release[65];
+    char sysname[65];
+    char version[65];
 };
 
 int uname(struct utsname *__name);
