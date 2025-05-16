2458377927afe0a6d6c1821f5157d97268f610eb
monkuous <84319635+monkuous@users.noreply.github.com>
2025-05-16 11:20:33 +0200

    time: implement clock and times

--- libc/sys/times/times.c
+++ libc/sys/times/times.c
@@ -1,6 +1,21 @@
 #include "sys/times.h"
 #include "compiler.h"
+#include <errno.h> /* IWYU pragma: keep */
+#include <hydrogen/process.h>
+#include <hydrogen/time.h>
 
 EXPORT clock_t times(struct tms *buffer) {
-    STUB();
+    hydrogen_cpu_time_t time;
+    int error = hydrogen_process_get_cpu_time(&time);
+
+    if (unlikely(error)) {
+        errno = error;
+        return -1;
+    }
+
+    buffer->tms_utime = time.user_time / 1000;
+    buffer->tms_stime = time.kernel_time / 1000;
+    buffer->tms_cutime = time.child_user_time / 1000;
+    buffer->tms_cstime = time.child_kernel_time / 1000;
+    return hydrogen_boot_time() / 1000;
 }
--- libc/time/clock.c
+++ libc/time/clock.c
@@ -1,6 +1,16 @@
 #include "compiler.h"
 #include "time.h"
+#include <errno.h> /* IWYU pragma: keep */
+#include <hydrogen/process.h>
 
 EXPORT clock_t clock(void) {
-    return -1;
+    hydrogen_cpu_time_t time;
+    int error = hydrogen_process_get_cpu_time(&time);
+
+    if (unlikely(error)) {
+        errno = error;
+        return -1;
+    }
+
+    return (time.kernel_time + time.user_time) / 1000;
 }
