a9dcdffce6d871d74de674498bb3866f63675004
monkuous <84319635+monkuous@users.noreply.github.com>
2025-05-21 21:39:43 +0200

    unistd: implement pipe

--- libc/unistd/pipe.c
+++ libc/unistd/pipe.c
@@ -1,6 +1,15 @@
 #include "compiler.h"
 #include "unistd.h"
+#include <errno.h> /* IWYU pragma: keep */
+#include <hydrogen/filesystem.h>
 
 EXPORT int pipe(int fildes[2]) {
-    STUB();
+    int error = hydrogen_fs_pipe(fildes, 0);
+
+    if (unlikely(error)) {
+        errno = error;
+        return -1;
+    }
+
+    return 0;
 }
